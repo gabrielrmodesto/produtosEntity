72900e998faa92c9186308bac298a329a20a4c8a
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-07-02 00:47:22 -0300

    Create a FK

--- Alura.Loja.Testes.ConsoleApp/Compra.cs
+++ Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -2,10 +2,9 @@
 {
     internal class Compra
     {
-        public Compra()
-        {
-        }
+        public int Id { get; set; }
 
+        public int ProdutoId { get; set; }
         public int Quantidade { get; internal set; }
         public Produto Produto { get; internal set; }
         public double Preco { get; internal set; }
--- Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -6,6 +6,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     internal class LojaContext : DbContext
     {
         public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
         public LojaContext() { }
         public LojaContext(DbContextOptions<LojaContext> options) : base(options) { }
 
