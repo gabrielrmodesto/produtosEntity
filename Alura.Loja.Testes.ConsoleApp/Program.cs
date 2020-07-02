d70be9ea0682a83f6501ee46266299b0d284247c
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-07-02 00:39:29 -0300

    Class compra

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -13,7 +13,17 @@ namespace Alura.Loja.Testes.ConsoleApp
 
         static void Main(string[] args)
         {
-            
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Frances";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
+
+            var compra = new Compra();
+
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
         }
         
 
